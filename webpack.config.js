5234606d2cf553290b66467f43cec2c2f952c0f0
Kristian Kaa <kaakristian@gmail.com>
2016-11-27 19:49:53 +0100

    Restructed files, add hot reload

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,31 @@
 var path = require('path');
+var webpack = require('webpack');
 
 module.exports = {
-  entry: './todo.js',
-  output: {
-    path: __dirname,
-    filename: 'bundle.js'
+  devtool: 'source-map',
+  debug: true,
+  entry: {
+    Todo1: [
+      'react-hot-loader/patch',
+      'webpack-hot-middleware/client',
+      './src/todo1'
+    ],
   },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].min.js',
+    publicPath: '/dist/'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
   module: {
     loaders: [
-      { test: /\.jsx?$/,
-        loader: 'babel?presets[]=es2015,presets[]=stage-0,presets[]=react' }
+      {
+        test: /\.jsx?$/,
+        loader: 'babel?presets[]=es2015,presets[]=stage-0,presets[]=react',
+        include: path.join(__dirname, 'src')
+      }
     ]
   }
 };
